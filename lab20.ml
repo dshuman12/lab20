67a70d7cc305335ffed58d2dd52b09bb1c318f0f
Daniela Shuman <d.shuman@comcast.net>
2020-04-23 10:58:36 -0700

    Initial commit

--- lab20.ml
+++ lab20.ml
@@ -2,20 +2,30 @@ type image = float list list ;;
 (* images are lists of lists of floats between 0. (white) and 1. (black) *)
 type size = int * int ;;
 open Graphics ;;
-  
+
 (* threshold thershold image -- image where pixels above the threshold
 value are black *)
 let threshold img threshold =
-  List.map  (fun row -> List.map (fun v -> if v <= threshold then 0. else 1.)
-                                 row) img
-       
+  let cBLACK = 1. in
+  let cWHITE = 0. in
+  List.map  (fun row ->
+    (*Sets the greyscale image pixels to be either white or black*)
+    List.map (fun fraction_grey ->
+      if fraction_grey <= threshold then cWHITE
+      else cBLACK) row) img
+
 (* show the image *)
 let depict img =
   Graphics.open_graph ""; Graphics.clear_graph ();
-  let x, y = List.length (List.hd img), List.length img in Graphics.resize_window x y;
-  let depict_pix v r c = let lvl = int_of_float (255. *. (1. -. v)) in Graphics.set_color (Graphics.rgb lvl lvl lvl);
-  plot c (y - r) in
-  List.iteri (fun r row -> List.iteri (fun c pix -> depict_pix pix r c) row) img;
+  let width, height = List.length (List.hd img), List.length img
+      in Graphics.resize_window width height;
+  let depict_pix fraction_grey r_index c_index =
+      let rgb_grey = int_of_float (255. *. (1. -. fraction_grey))
+      in Graphics.set_color (Graphics.rgb rgb_grey rgb_grey rgb_grey);
+      plot rgb_grey (height - r_index) in
+  (*Runs depict_pix on each pixel, drawing row by row*)
+  List.iteri (fun r_index row ->
+  List.iteri (fun c_index pix -> depict_pix pix r_index c_index) row) img;
   Unix.sleep 2; Graphics.close_graph () ;;
 
 (* dither max image -- dithered image *)
@@ -27,14 +37,14 @@ let dither img =
                  then 1.
                  else 0.) row)
     img
-    
+
 let mona = Monalisa.image ;;
-  
+
   depict mona ;;
-    
+
   let mona_threshold = threshold mona 0.75 ;;
     depict mona_threshold ;;
-      
+
     let mona_dither = dither mona ;;
       depict mona_dither ;;
-           
+
